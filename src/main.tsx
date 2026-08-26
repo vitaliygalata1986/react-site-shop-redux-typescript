c931d66191ae7728bcece6e7c554621d29869f09
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2026-08-26 13:06:50 +0300

    refactor: move product fetching into page component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { lazy, Suspense } from 'react';
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, defer, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Layout } from './layout/Menu/Layout';
 import { Error as ErrorPage } from './pages/Error/Error';
 import { Product } from './pages/Product/Product';
 import './index.css';
-import axios from 'axios';
-import { PREFIX } from './api/api';
 import AuthLayout from './layout/Auth/AuthLayout';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
@@ -35,17 +33,17 @@ const router = createBrowserRouter(
         {
           path: '/product/:id',
           element: <Product />,
-          errorElement: <>Ошибка</>,
-          loader: async ({ params }) => {
-            return defer({
-              data: axios
-                .get(`${PREFIX}/products/${params.id}`)
-                .then((data) => data)
-                .catch((error) => {
-                  throw new Error(error);
-                }),
-            });
-          },
+          // errorElement: <>Ошибка</>
+          // loader: async ({ params }) => {
+          //   return defer({
+          //     data: axios
+          //       .get(`${PREFIX}/products/${params.id}`)
+          //       .then((data) => data)
+          //       .catch((error) => {
+          //         throw new Error(error);
+          //       }),
+          //   });
+          // },
         },
       ],
     },
@@ -88,13 +86,13 @@ const router = createBrowserRouter(
   ],
   {
     basename: '/react-site-shop-redux-typescript/',
-  }
+  },
 );
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <RouterProvider router={router} />
-  </Provider>
+  </Provider>,
 );
 
 /*
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,101 +1,149 @@
 import styles from './Product.module.css';
-import { Await, useLoaderData, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 // import { Await, useLoaderData } from 'react-router-dom';
 import { IProduct } from '../../interfaces/product.interface';
-import { Suspense } from 'react';
+import { useEffect, useState } from 'react';
 import Heading from '../../components/Headling/Headling';
 import Button from '../../components/Button/Button';
 import { useDispatch } from 'react-redux';
 import { add } from '../../store/cart.slice';
 import { AppDispath } from '../../store/store';
+import axios from 'axios';
+import { PREFIX } from '../../api/api';
 // import { Suspense } from 'react';
 
 export function Product() {
   // const data = useLoaderData() as IProduct; // приведем useLoaderData к одному продукту, потому, что useLoaderData - unknown
-  const data = useLoaderData() as { data: IProduct }; // так как мы уже возвращаем объект data: axios.get(`${PREFIX}/products/${params.id}`).then((data) => data)
+  // const data = useLoaderData() as { data: IProduct }; // так как мы уже возвращаем объект data: axios.get(`${PREFIX}/products/${params.id}`).then((data) => data)
   // throw new Error('error');
   // мы получаем data, когда промис развезолвится
   // перед тем, как получить данные - нужно что-то показать
+  const { id } = useParams();
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispath>();
+  const [product, setProduct] = useState<IProduct>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  useEffect(() => {
+    if (!id) {
+      setError('Не найден id продукта');
+      return;
+    }
+
+    const getProduct = async () => {
+      try {
+        setIsLoading(true);
+        setError(undefined);
+        const { data } = await axios.get<IProduct>(`${PREFIX}/products/${id}`);
+
+        setProduct(data);
+      } catch (e) {
+        console.error(e);
+        if (axios.isAxiosError(e)) {
+          setError(e.message);
+        } else if (e instanceof Error) {
+          setError(e.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getProduct();
+  }, [id]);
+
+  if (isLoading) {
+    return <>Загружаю...</>;
+  }
+
+  if (error) {
+    return <>Ошибка: {error}</>;
+  }
+
+  if (!product) {
+    return <>Продукт не найден</>;
+  }
 
   return (
-    <>
-      <Suspense fallback={'Загружаю...'}>
-        <Await resolve={data.data}>
-          {(
-            { data }: { data: IProduct } // получаем продукт
-          ) => (
-            <div className={styles['product-block']}>
-              <div className={styles['product-top']}>
-                <div className={styles['product-top-first']}>
-                  <button
-                    className={styles['product-prev']}
-                    onClick={() => navigate(-1)}
-                  >
-                    <img
-                      src={`${import.meta.env.BASE_URL}button_left.svg`}
-                      alt='prev'
-                    />
-                  </button>
-                  <Heading>{data.name}</Heading>
-                </div>
-                <Button
-                  appearence='big'
-                  className={styles['product-buy']}
-                  onClick={() => dispatch(add(data.id))}
-                >
-                  <img
-                    src={`${import.meta.env.BASE_URL}cart-icon-white.svg`}
-                    alt='cart icon'
-                  />
-                  В корзину
-                </Button>
-              </div>
-              <div className={styles['product-bottom']}>
-                <div className={styles['product-bottom__left']}>
-                  <img
-                    className={styles['product-bottom__image']}
-                    src={data.image}
-                    alt={data.name}
-                  />
-                </div>
-                <div className={styles['product-bottom__right']}>
-                  <div className={styles['product-bottom__right-top']}>
-                    Цена
-                    <div>
-                      <span className={styles['product-price']}>
-                        {data.price}
-                      </span>
-                      <span className={styles['product-valute']}> грн.</span>
-                    </div>
-                  </div>
-                  <div
-                    className={`${styles['product-bottom__right-top']} ${styles['product-bottom__right-top-bg']}`}
-                  >
-                    Рейтинг
-                    <span className={styles['product-rayting']}>
-                      {data.rating}
-                      <img
-                        src={`${import.meta.env.BASE_URL}cart-button-icon.svg`}
-                        alt='Иконка звезды'
-                      />
-                    </span>
-                  </div>
-                  <div className={styles['product-bottom__right-bottom']}>
-                    Состав:
-                    <ul className={styles['product-bottom__ingredient']}>
-                      {data.ingredients.map((ingredient, index) => (
-                        <li key={index}>{ingredient}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              </div>
+    <div className={styles['product-block']}>
+      <div className={styles['product-top']}>
+        <div className={styles['product-top-first']}>
+          <button
+            className={styles['product-prev']}
+            onClick={() => navigate(-1)}
+          >
+            <img
+              src={`${import.meta.env.BASE_URL}button_left.svg`}
+              alt="prev"
+            />
+          </button>
+          <Heading>{product.name}</Heading>
+        </div>
+        <Button
+          appearence="big"
+          className={styles['product-buy']}
+          onClick={() => dispatch(add(product.id))}
+        >
+          <img
+            src={`${import.meta.env.BASE_URL}cart-icon-white.svg`}
+            alt="cart icon"
+          />
+          В корзину
+        </Button>
+      </div>
+      <div className={styles['product-bottom']}>
+        <div className={styles['product-bottom__left']}>
+          <img
+            className={styles['product-bottom__image']}
+            src={product.image}
+            alt={product.name}
+          />
+        </div>
+        <div className={styles['product-bottom__right']}>
+          <div className={styles['product-bottom__right-top']}>
+            Цена
+            <div>
+              <span className={styles['product-price']}>{product.price}</span>
+              <span className={styles['product-valute']}> грн.</span>
             </div>
-          )}
-        </Await>
-      </Suspense>
-    </>
+          </div>
+          <div
+            className={`${styles['product-bottom__right-top']} ${styles['product-bottom__right-top-bg']}`}
+          >
+            Рейтинг
+            <span className={styles['product-rayting']}>
+              {product.rating}
+              <img
+                src={`${import.meta.env.BASE_URL}cart-button-icon.svg`}
+                alt="Иконка звезды"
+              />
+            </span>
+          </div>
+          <div className={styles['product-bottom__right-bottom']}>
+            Состав:
+            <ul className={styles['product-bottom__ingredient']}>
+              {product.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
+
+/*
+useCallback нужен в основном из-за useEffect:
+- если id не изменился, getProduct остаётся той же функцией
+- значит useEffect не запускается заново из-за новой функции
+- значит лишнего запроса не буде
+
+
+Можно даже убрать id из зависимостей useEffect, потому что id уже есть в зависимостях useCallback. 
+Когда id изменится, изменится getProduct, и useEffect запустится.
+Итого:
+- useCallback(..., [id]) говорит: пересоздай getProduct, только если изменился id
+- useEffect(..., [getProduct]) говорит: запусти эффект, если изменилась getProduct
+*/
